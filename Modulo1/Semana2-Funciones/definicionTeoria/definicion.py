2146e7eb1a0b761cd8fd180fe8ac6299606bced9
¨telma¨ <jutelma07051980@gmail.com>
2023-09-22 14:11:46 +0200

    Creando directorios y hojas de trabajo

---
+++ Modulo1/Semana2-Funciones/definicionTeoria/definicion.py
@@ -0,0 +1,14 @@
+"""Resumen de la definición de funciones
+Analizamos algunos ejemplos de funciones integradas en Python, pero poder definir tus propias funciones es
+increíblemente poderoso. Comenzamos la definición de una función con la palabra clave def, seguida del nombre que
+queremos darle a nuestra función.
+Después del nombre, tenemos los parámetros, también llamados argumentos, de la función entre paréntesis. Una función
+puede no tener parámetros o puede tener varios parámetros. Los parámetros nos permiten llamar a una función y pasarle
+datos,y los datos están disponibles dentro de la función como variables con el mismo nombre que los parámetros.
+Por último, ponemos dos puntos al final de la línea.
+Después del colon, comienza el cuerpo funcional. Es importante tener en cuenta que en Python el cuerpo de la función
+está delimitado por sangría. Esto significa que todo el código con sangría a la derecha después de la definición de una
+función es parte del cuerpo de la función. La primera línea que ya no tiene sangría es el límite del cuerpo de la
+función.
+Depende de usted cuántos espacios utilice al aplicar sangría; solo asegúrese de ser coherente. Entonces, si elige
+sangrar con cuatro espacios, deberá utilizar cuatro espacios en todas partes de su código."""
---
+++ Modulo1/Semana2-Funciones/definicionTeoria/valoresDevueltos.py
@@ -0,0 +1,7 @@
+    def area_triangle(base, heigth):
+            return base * heigth/2
+
+    area_a=area_triangle(5,4)
+    area_b = area_triangle (7, 3)
+    sum = area_a + area_b
+    print("the sum of both areas is: " + str(sum))
