9c8939db411490d4130236ac3740ab565e405a26
¨telma¨ <jutelma07051980@gmail.com>
2023-09-23 07:47:35 +0200

    reutilizacion de codigo

---
+++ Modulo1/Semana2-Funciones/definicionTeoria/reutilizacionDeCodigo.py
@@ -0,0 +1,9 @@
+name ="Kay"
+number =len(name) * 9
+
+print("hello" + * name + " . your lucky number is " + str(number))
+
+name = "Cameron"
+number = len(name) *9
+
+print("hello" + name + ". Your lucky number is ") + str(number))
