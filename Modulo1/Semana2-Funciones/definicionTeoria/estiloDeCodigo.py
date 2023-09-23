4e2da327a8f0e6b2c38c7173bf2e1fa038a532e9
¨telma¨ <jutelma07051980@gmail.com>
2023-09-23 08:43:15 +0200

    estilo de codigo

---
+++ Modulo1/Semana2-Funciones/definicionTeoria/estiloDeCodigo.py
@@ -0,0 +1,6 @@
+def calculate(d):
+    q = 3.14
+    z = q * (d ** 2)
+    print(z)
+
+calculate(5)
--- Modulo1/Semana2-Funciones/definicionTeoria/reutilizacionDeCodigo.py
+++ Modulo1/Semana2-Funciones/definicionTeoria/reutilizacionDeCodigo.py
@@ -1,9 +1,22 @@
-name ="Kay"
-number =len(name) * 9
+"""ejemplo : Este script utiliza la función len, que devuelve la longitud de una cadena. En este ejemplo, el script
+    utiliza esa longitud para calcular un número, que estamos llamando al número de la suerte aquí. Y finalmente, imprime
+    un mensaje con el nombre y el número."""
 
-print("hello" + * name + " . your lucky number is " + str(number))
+name = "Kay"
+number = len(name) * 9
+
+print("hello " + name + ". your lucky number is " + str(number))
 
 name = "Cameron"
 number = len(name) *9
 
-print("hello" + name + ". Your lucky number is ") + str(number))
+print("hello " + name + " . Your lucky number is " + str(number))
+
+"""¿Qué tal si reescribimos este código creando una función para agrupar todo el código duplicado?"""
+
+def lucky_number(name):
+    number = len(name) * 9
+    print ("hello " + name + " . your lucky number is " + str(number))
+
+lucky_number("Kay")
+lucky_number("Caameron")
