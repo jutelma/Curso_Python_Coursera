c54840dc619c2ee14569adda75fbb735e0259b8f
¨telma¨ <jutelma07051980@gmail.com>
2023-12-29 00:38:07 +0100

    Glosario de terminos del curso 2

---
+++ Modulo2/glossaryTermsFromCourse2.py
@@ -0,0 +1,29 @@
+"""
+Glosario de términos del curso 2, módulo 1
+Términos y definiciones del Curso 2, Módulo 1
+Lenguaje compilado o compilado: Código compatible que se puede ejecutar en diferentes plataformas
+
+Entorno de desarrollo integrado (IDE): un editor de código con capacidades adicionales para simplificar la escritura de
+guiones.
+
+Intérprete o lenguaje interpretado: un programa intermediario utilizado para ejecutar las instrucciones especificadas
+en el código.
+
+Kernel: el núcleo principal de un sistema operativo que se comunica directamente con el hardware y administra
+los recursos del sistema.
+
+Linux: un sistema operativo de código abierto donde el software se puede compartir, modificar y distribuir de forma
+gratuita.
+
+Mac OS: Sistema operativo desarrollado por Apple
+
+Sistema operativo (SO): Software que gestiona todo lo que sucede en la computadora, compuesto por dos partes
+principales: el kernel y el espacio de usuario.
+
+Espacio de usuario:
+Todo lo que está fuera del kernel con el que los usuarios interactúan directamente.
+
+Entorno virtual: una herramienta que te permite crear entornos aislados para tus proyectos Python
+
+SO Windows: Sistema operativo desarrollado por Microsoft
+"""
