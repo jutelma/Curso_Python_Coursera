3b8b4e3680237b6589ce0b4a2c5c37c906d7d38e
¨telma¨ <jutelma07051980@gmail.com>
2023-12-29 00:25:27 +0100

    Prueba de practica hecha

---
+++ Modulo2/practiceQuizAutomation.py
@@ -0,0 +1,39 @@
+# Pregunta 1 En una planta de fabricación, un empleado dedica varios minutos cada hora a anotar el tiempo de
+# actividad y el tiempo de inactividad de cada una de las máquinas que está ejecutando. ¿Cuál de las siguientes ideas
+# automatizaría mejor este proceso?
+
+# R Agregue un módulo analógico de Internet de las cosas (IoT) a cada máquina para detectar sus estados de energía y
+# escriba un script que registre el tiempo de actividad y el tiempo de inactividad, informando cada hora.
+
+# Pregunta 2 Un aspecto importante de la automatización es el valor forense. ¿Cuál de las siguientes afirmaciones
+# describe correctamente este término?
+
+# R Es importante que los procesos automatizados dejen registros extensos para que, cuando se produzcan errores,
+# se puedan investigar adecuadamente.
+
+# Pregunta 3 Un empleado de una empresa de soporte técnico debe recopilar informes en un solo archivo y enviarlo por
+# correo electrónico tres veces al día, cinco días a la semana durante un mes, además de sus otras tareas. Supongamos
+# que un mes son cuatro semanas y que le llevará 10 horas codificar el script de automatización. Usando la fórmula [
+# time_to_automate < (time_to_perform * cantidad_de_veces_hechas)], ¿cuánto tiempo les tomaría en minutos antes de
+# ahorrar tiempo en el proceso?
+
+# R 100 minutos
+
+# Pregunta 4
+# Una empresa está considerando automatizar uno de sus procesos internos y quiere determinar si
+# automatizar un proceso ahorraría tiempo de mano de obra este año. La empresa utiliza la fórmula [time_to_automate <
+# (time_to_perform * cantidad_de_veces_hechas)] para decidir si vale la pena la automatización. El proceso
+# normalmente dura unos 10 minutos cada semana. El proceso de automatización en sí tardará 40 horas en total en
+# completarse. Usando la fórmula, ¿cuántas semanas pasarán antes de que la empresa comience a ahorrar tiempo en el
+# proceso?
+
+# R 240 semanas
+
+# Pregunta 5
+# ¿Cuáles de los siguientes son métodos válidos para evitar errores de automatización silenciosa?
+
+# R Comprobaciones periódicas de coherencia
+#   Notificaciones por correo electrónico sobre errores
+#   Entradas de seguimiento de problemas internos
+
+
