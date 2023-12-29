69a5d59835e17aa2b4c75472c867324b5bce4f14
¨telma¨ <jutelma07051980@gmail.com>
2023-12-29 01:37:28 +0100

    Evaluacion calificada modulo 1 aprobada

---
+++ Modulo2/gradedAssessmentModule1.py
@@ -0,0 +1,81 @@
+"""
+
+Pregunta 1
+¿Cuál de las siguientes afirmaciones describe con mayor precisión lo que hace un sistema operativo? Seleccione todas
+las que correspondan.
+
+El sistema operativo es un software que gestiona todo lo que sucede en la computadora. Lee, escribe y elimina archivos
+del disco duro. Maneja cómo comienzan los procesos, cómo interactúan entre sí y cómo terminan finalmente. Gestiona
+cómo se asigna la memoria a diferentes procesos, cómo se envían y reciben los paquetes de red y cómo cada programación
+puede acceder a los diferentes componentes de hardware.
+
+
+
+R Gestiona cómo se asigna la memoria a diferentes procesos.
+  Maneja cómo los procesos comienzan, interactúan entre sí y finalizan.
+
+2 ¿En qué sistema operativo puedes usar Python? Seleccione todas las que correspondan.
+
+R Sistema operativo Linux
+  Sistema operativo Windows
+  Mac OS
+
+Pregunta 3
+¿Qué comando debería escribir en la terminal de su computadora para verificar si Python está instalado?
+
+R Python --version
+
+Pregunta 4
+¿Cuál de las siguientes afirmaciones describe mejor un lenguaje interpretado?
+
+R Un lenguaje interpretado es un lenguaje de programación que un intérprete ejecuta línea por línea.
+
+Pregunta 5
+Los IDE pueden resultar muy útiles para los desarrolladores de software, ya que pueden proporcionar una variedad de
+herramientas que pueden ahorrar tiempo y mejorar la productividad. ¿Qué herramientas proporcionan los IDE? Seleccione
+todas las que correspondan.
+
+R editor de código
+  Depurador
+  Compilador
+
+Pregunta 6
+Eres un desarrollador de software que trabaja en un nuevo proyecto de Python. Estás desarrollando una aplicación web.
+Utilizará una variedad de bibliotecas de Python. Está empezando a preocuparse por el impacto de su proyecto en el
+entorno Python de su sistema. Le preocupa que la instalación de las bibliotecas para su proyecto entre en conflicto con
+las bibliotecas que está utilizando para otros proyectos. Has oído hablar de los entornos virtuales y te preguntas si
+podrían ser una solución a tu problema. ¿Cuál de las siguientes afirmaciones describe mejor cómo los entornos virtuales
+pueden ayudarle a resolver su problema?
+
+R Los entornos virtuales le permiten instalar bibliotecas de forma aislada del entorno Python de su sistema.
+
+Pregunta 7
+Usted es un gerente de marketing ocupado en una empresa emergente de rápido crecimiento. Constantemente envía correos
+electrónicos para promocionar los productos y servicios de su empresa. Sin embargo, descubre que esta tarea le consume
+mucho tiempo. ¿Qué podrías implementar para ahorrar tiempo en esta tarea?
+
+R Automatización
+
+Pregunta 8
+En ocasiones, los sistemas de automatización pueden experimentar fallas, lo que interrumpe las operaciones y provoca
+la pérdida de datos. ¿Qué estrategias se pueden implementar para garantizar una notificación oportuna y una pronta
+solución en caso de fallas del sistema de automatización?
+
+R Establecer un sistema de monitoreo para detectar y notificar al personal relevante sobre fallas en el sistema de
+automatización.
+
+
+Pregunta 9
+Quiere crear un script de Python que automatice las tareas de monitoreo del sistema en su computadora. ¿Qué módulo de
+Python deberías usar en tu script?
+
+R psutil
+
+Pregunta 10
+Una empresa bancaria busca automatizar uno de sus procesos internos que tarda unos 50 minutos cada semana en
+completarse. El proceso de automatización tardará 12 horas en total en completarse. ¿Cuál es la fórmula utilizada para
+determinar cuántas semanas tomará el proceso?
+
+R 600 < 50x
+
+"""
