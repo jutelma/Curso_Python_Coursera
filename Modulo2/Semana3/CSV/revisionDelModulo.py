db08af555afd6ff3e27bc8440bd164a46f904867
¨telma¨ <jutelma07051980@gmail.com>
2024-01-04 01:40:34 +0100

    revision del modulo quilabs

---
+++ Modulo2/Semana3/CSV/revisionDelModulo.py
@@ -0,0 +1,35 @@
+"""
+Cómo iniciar sesión en Qwiklabs
+
+En las siguientes evaluaciones, utilizará Qwiklabs para el aprendizaje práctico. Qwiklabs proporciona recursos
+respaldados por Google Cloud que se utilizarán para realizar las tareas de las evaluaciones. Al utilizar Qwiklabs,
+no tendrá que comprar ni instalar software usted mismo y podrá utilizar el sistema operativo Linux como si estuviera
+instalado en su máquina local.
+
+Detalles importantes:
+
+    · Tendrá 90 minutos para completar cada laboratorio.
+
+    · Experimentará un retraso a medida que se cargan los laboratorios, así como también para las instancias de trabajo
+      de las máquinas virtuales Linux. Entonces, espere un par de minutos.
+
+    · Asegúrese de acceder a los laboratorios directamente a través de Coursera y no desde el catálogo de Qwiklabs.
+      Si accede a los laboratorios a través del catálogo de Qwiklabs, no recibirá una calificación. (Como usted sabe,
+      se requiere una calificación aprobatoria para completar el curso).
+
+    · Se conectará a una nueva máquina virtual para cada laboratorio con credenciales temporales creadas para usted;
+      estos durarán solo mientras dure el laboratorio.
+
+    · La calificación se calcula cuando se completa la práctica de laboratorio, así que asegúrese de presionar
+      "Finalizar práctica de laboratorio" cuando haya terminado. Nota: una vez finalizada la práctica de laboratorio,
+      no podrá acceder a su trabajo anterior.
+
+    · Para familiarizarse con el ingreso a las prácticas de laboratorio, busque los enlaces a continuación para el
+      sistema operativo de la máquina que está utilizando actualmente  para visualizar los pasos clave.
+      Tenga en cuenta que, si bien los recursos de video vinculados a continuación no tienen voz en off ni audio,
+      todos los detalles importantes seguirán alojados en el conjunto de instrucciones de cada laboratorio en
+      la plataforma Qwiklabs.
+
+    · Si recibe el error " Lo sentimos, se superó su cuota para el laboratorio ", por favor
+      presentar una solicitud o comuníquese con el equipo de soporte de Qwiklabs directamente a través del chat en qwiklabs.com.
+"""
