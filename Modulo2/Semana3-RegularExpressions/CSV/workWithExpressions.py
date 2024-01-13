94dbd275fe829cbfae219ee087310669af68b43e
¨telma¨ <jutelma07051980@gmail.com>
2024-01-13 16:33:50 +0100

    Examen teorico expresiones regulares enfocando el laboratorio anterio

---
+++ Modulo2/Semana3-RegularExpressions/CSV/workWithExpressions.py
@@ -0,0 +1,62 @@
+# Pregunta 1
+# ¿Qué es una expresión regular?
+
+# R Una secuencia de caracteres que forma un patrón de búsqueda.
+
+# Pregunta 2
+# ¿Qué método del módulo csv se utiliza para escribir filas de datos en un archivo CSV?
+
+# R csv.escritores()
+
+# Pregunta 3
+# En el script de Python para procesar user_emails.csv , ¿cuál es el propósito de la función contains_domain ?
+
+# R Para comprobar si una dirección de correo electrónico pertenece a un dominio específico, utilice expresiones
+# regulares (RegEx)
+
+# Pregunta 4
+# ¿Qué función se utiliza para hacer coincidir un patrón de expresión regular en Python?
+
+# R Todo lo anterior
+
+# .
+# Pregunta 5
+# ¿Cuál es el significado de las variables csv_file_location y report_file en el fragmento de código del laboratorio?
+# csv_file_location = '<csv-file-location>'
+# report_file = '<data-directory>' + '/updated_user_emails.csv'
+
+# R Almacenan rutas de archivos para archivos CSV de entrada y salida.
+
+# Pregunta 6
+# ¿Cuál es el propósito de la función replace_domain en el proceso de reemplazar nombres de dominio antiguos por
+# otros nuevos en un archivo CSV usando Python?
+
+# R Para reemplazar el dominio antiguo con el nuevo dominio en una dirección de correo electrónico
+
+# Pregunta 7
+# ¿Cuál es el propósito principal de escribir la lista de datos de usuario actualizada en un archivo de salida en
+# el proceso de reemplazar nombres de dominio antiguos por otros nuevos en un archivo CSV usando Python, como se
+# describe en la práctica de laboratorio?
+
+# R Para guardar los cambios realizados en las direcciones de correo electrónico y crear un nuevo archivo CSV con
+# datos actualizados
+
+# Pregunta 8
+# ¿Por qué es importante reemplazar los nombres de dominio antiguos por otros nuevos y generar un archivo nuevo
+# que contenga todos los nombres de usuario con sus respectivas direcciones de correo electrónico?
+
+# R Para mejorar las medidas de seguridad de los datos.
+
+# ¿Por qué es importante definir encabezados para el archivo de salida al reemplazar nombres de dominio
+# antiguos por otros nuevos en un archivo CSV usando Python, como se describe en la práctica de laboratorio?
+
+# R Para identificar la columna que contiene direcciones de correo electrónico en el archivo de salida
+
+# 10.
+# Pregunta 10
+# Se le ha asignado la tarea de reemplazar nombres de dominio antiguos por otros nuevos en un archivo CSV
+# usando Python, según la práctica de laboratorio. ¿Cuál es la secuencia correcta de pasos para realizar esta tarea?
+
+# R Inicialice user_email_list , defina encabezados para el archivo de salida, escriba la lista de datos de usuario
+# actualizada en el archivo de salida y cierre el archivo CSV.
+
